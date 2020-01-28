f353b1f6cb86c908b87e5cb959ad3d6998301aa0
Tyler Solosko <TSolo215@live.com>
2020-01-28 16:05:11 -0700

    Added a settings menu and add user functionality.

--- database_management.py
+++ database_management.py
@@ -17,7 +17,6 @@ class DatabaseManager:
         for row in self.cursor.execute('''SELECT product_name
                             FROM product'''):
             name_list.append(row[0])
-        print(name_list)
         return name_list
 
     def get_store_list(self):
@@ -27,11 +26,16 @@ class DatabaseManager:
             store_list.append(" - ".join(row[0:2]))
         return store_list
 
+    def get_user_list(self):
+        user_list = []
+        for row in self.cursor.execute('''SELECT username_name
+                            FROM username'''):
+            user_list.append(row[0])
+        return user_list
+
     def update_product(self, product_detail_list):
         product_name = product_detail_list[0]
         product_price = product_detail_list[1]
-        product_category = product_detail_list[3]
-        product_weight = product_detail_list[4]
         self.cursor.execute('''UPDATE product
          SET product_times_purchased = product_times_purchased + 1,
              product_highest_price = CASE WHEN product_highest_price < ? THEN (?) ELSE product_highest_price END,
@@ -49,4 +53,25 @@ class DatabaseManager:
           VALUES (?,?,?,?,?,?,?)''', (product_name, product_price, product_price, product_price, 1, product_category, product_weight))
 
     def push_transaction(self, product_detail_list, username, store, date):
-        pass
+        store = store.split(" - ")
+        store_name = store[0]
+        store_city = store[1]
+        product_name = product_detail_list[0]
+        product_price = product_detail_list[1]
+        product_quantity = product_detail_list[2]
+        self.cursor.execute('''INSERT INTO store_transaction
+                 (username_id, store_id, product_id, transaction_price, transaction_quantity, transaction_date) VALUES ((SELECT username_id FROM username WHERE username_name = ?),
+                  (SELECT store_id FROM store WHERE store_name = ? AND store_city = ?), (SELECT product_id FROM product WHERE product_name = ?),?,?,?)''',
+                            (username, store_name, store_city, product_name, product_price, product_quantity, date))
+
+    def push_transaction_summary(self, username, store, date, total):
+        store = store.split(" - ")
+        store_name = store[0]
+        store_city = store[1]
+        self.cursor.execute('''INSERT INTO transaction_summary
+                        (username_id, store_id, summary_date, summary_total) VALUES ((SELECT username_id FROM username WHERE username_name = ?),
+                         (SELECT store_id FROM store WHERE store_name = ? AND store_city = ?),?,?)''',
+                            (username, store_name, store_city, date, total))
+
+    def push_new_user(self, new_username):
+        self.cursor.execute('''INSERT INTO username (username_name) VALUES (?)''', (new_username,))
--- gui_initiation.py
+++ gui_initiation.py
@@ -10,13 +10,15 @@ class MainGUI:
     def __init__(self, master, settings_manager, database_manager):
         self.master = master
         self.DBM = database_manager
+        self.SM = settings_manager
+
         self.name_list = self.DBM.get_product_name_list()
         self.store_list = self.DBM.get_store_list()
+        self.username_list = self.DBM.get_user_list()
         self.transaction_dict = {}
         self.today_date = datetime.today().strftime("%Y-%m-%d")
 
-        self.active_user = settings_manager.active_user
-        print(self.active_user)
+        self.active_user = self.SM.active_user
 
         master.title("Grocery Tracker: " + self.active_user)
         master.geometry("1200x800")
@@ -25,25 +27,27 @@ class MainGUI:
 
         self.mainframe = Frame(self.master, height=1200, width=800, bg=main_background_color)
         self.mainframe.grid(row=0, column=0, rowspan=10, columnspan=20, sticky=W + E + N + S, padx=0, pady=0)
+        self.settings_button = Button(self.mainframe, text="Settings", font="helvetica", command=self.open_settings_menu)
+        self.settings_button.grid(row=0, column=0, columnspan=2, sticky=W, padx=(10, 0), pady=(10, 0))
         self.item_label = Label(self.mainframe, text='Item:', bg=main_background_color, fg=main_foreground_color)
-        self.item_label.grid(row=1, column=0, sticky=W, padx=(10, 0), pady=(10, 5))
+        self.item_label.grid(row=1, column=0, sticky=W, padx=(10, 0), pady=(5, 5))
         self.item_entry = autocomplete_entry_widget.AutocompleteEntry(self.name_list, self.mainframe)
-        self.item_entry.grid(row=1, column=1, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.item_entry.grid(row=1, column=1, sticky=W, padx=(0, 0), pady=(5, 5))
         self.price_label = Label(self.mainframe, text='Price:', bg=main_background_color, fg=main_foreground_color)
-        self.price_label.grid(row=1, column=2, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.price_label.grid(row=1, column=2, sticky=W, padx=(0, 0), pady=(5, 5))
         self.price_entry = Entry(self.mainframe)
-        self.price_entry.grid(row=1, column=3, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.price_entry.grid(row=1, column=3, sticky=W, padx=(0, 0), pady=(5, 5))
         self.quantity_label = Label(self.mainframe, text='Quantity:', bg=main_background_color, fg=main_foreground_color)
-        self.quantity_label.grid(row=1, column=4, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.quantity_label.grid(row=1, column=4, sticky=W, padx=(0, 0), pady=(5, 5))
         self.quantity_entry = Entry(self.mainframe)
-        self.quantity_entry.grid(row=1, column=5, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.quantity_entry.grid(row=1, column=5, sticky=W, padx=(0, 0), pady=(5, 5))
         self.quantity_entry.insert(0, '1')
         self.weight_label = Label(self.mainframe, text='Weight (lb):', bg=main_background_color, fg=main_foreground_color)
-        self.weight_label.grid(row=1, column=6, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.weight_label.grid(row=1, column=6, sticky=W, padx=(0, 0), pady=(5, 5))
         self.weight_entry = Entry(self.mainframe)
-        self.weight_entry.grid(row=1, column=7, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.weight_entry.grid(row=1, column=7, sticky=W, padx=(0, 0), pady=(5, 5))
         self.category_label = Label(self.mainframe, text='Category:', bg=main_background_color, fg=main_foreground_color)
-        self.category_label.grid(row=1, column=8, sticky=W, padx=(0, 0), pady=(10, 5))
+        self.category_label.grid(row=1, column=8, sticky=W, padx=(0, 0), pady=(5, 5))
         self.category_box = Combobox(self.mainframe, textvariable='item_category', state="readonly")
         self.category_box['values'] = ('0.5 seconds', '1 seconds', '2 seconds', '3 seconds', '4 seconds', '5 seconds', '10 seconds', '20 seconds', '30 seconds', '60 seconds')
         self.category_box.current(0)
@@ -80,6 +84,89 @@ class MainGUI:
             self.mainframe.grid_rowconfigure(i, weight=1)
 
         master.update()
+
+    def open_settings_menu(self):
+
+        def add_user_menu():
+
+            def add_new_user():
+                new_username = username_entry.get()
+                if not new_username:
+                    return  # Error Message?
+                self.DBM.push_new_user(new_username)
+                self.DBM.save()
+                self.username_list.append(new_username)
+                if username_box:
+                    username_box['values'] = self.username_list
+                    username_box.current(len(self.username_list) - 1)
+                au_menu.destroy()
+
+            def close_new_user_menu():
+                au_menu.destroy()
+
+            xx = self.master.winfo_x()
+            yy = self.master.winfo_y()
+            dxx = 30
+            dyy = 30
+            au_menu = Toplevel(height=400, width=400)
+            au_menu.transient(settings_menu)
+            au_menu.title("Add User")
+            au_menu.update()
+            au_menu.geometry("+%d+%d" % (xx + dxx, yy + dyy))
+
+            username_label = Label(au_menu, text='Username:')
+            username_label.grid(row=0, column=0, sticky=W, padx=(10, 5), pady=(10, 0))
+            username_entry = Entry(au_menu)
+            username_entry.grid(row=0, column=1, sticky=W, padx=(5, 5), pady=(10, 0))
+            username_buttons_frame = Frame(au_menu)
+            username_buttons_frame.grid(row=1, column=0, columnspan=2, sticky=W + E + N + S, padx=0, pady=0)
+            add_new_user_button = Button(username_buttons_frame, text="Add User", command=add_new_user)
+            add_new_user_button.grid(row=0, column=0, sticky=W, padx=(10, 5), pady=10)
+            close_new_user_button = Button(username_buttons_frame, text="Close", command=close_new_user_menu)
+            close_new_user_button.grid(row=0, column=1, sticky=W, padx=(5, 5), pady=10)
+
+        def add_store_menu():
+            pass
+
+        def save_settings():
+            pass
+
+        def close_settings_menu():
+            settings_menu.destroy()
+
+        color_list = ['Red', 'Blue', 'Black', 'Grey', 'Orange', 'Pink', 'Green', 'Purple']
+        x = self.master.winfo_x()
+        y = self.master.winfo_y()
+        dx = 30
+        dy = 30
+        settings_menu = Toplevel(height=400, width=400)
+        settings_menu.transient(self.master)
+        settings_menu.title("Settings")
+        settings_menu.update()
+        settings_menu.geometry("+%d+%d" % (x + dx, y + dy))
+
+        userbox_label = Label(settings_menu, text='User:')
+        userbox_label.grid(row=0, column=0, sticky=W, padx=(10, 5), pady=(10, 0))
+        username_box = Combobox(settings_menu, textvariable='username', state="readonly")
+        username_box['values'] = self.username_list
+        username_box.current(0)
+        username_box.grid(row=0, column=1, sticky=W, padx=(0, 10), pady=(10, 10))
+        background_color_label = Label(settings_menu, text='Background Color:')
+        background_color_label.grid(row=1, column=0, sticky=W, padx=(10, 5), pady=(10, 0))
+        background_color_box = Combobox(settings_menu, textvariable='bgcolor', state="readonly")
+        background_color_box['values'] = color_list
+        background_color_box.current(0)
+        background_color_box.grid(row=1, column=1, sticky=W, padx=(0, 10), pady=(10, 10))
+        settings_button_frame = Frame(settings_menu)
+        settings_button_frame.grid(row=2, column=0, columnspan=2, sticky=W + E + N + S, padx=0, pady=0)
+        save_settings_button = Button(settings_button_frame, text="Save", command=save_settings)
+        save_settings_button.grid(row=0, column=0, sticky=W, padx=(10, 5), pady=(5, 10))
+        add_user_button = Button(settings_button_frame, text="Add User", command=add_user_menu)
+        add_user_button.grid(row=0, column=1, sticky=W, padx=(5, 5), pady=(5, 10))
+        add_store_button = Button(settings_button_frame, text="Add Store", command=add_store_menu)
+        add_store_button.grid(row=0, column=2, sticky=W, padx=(5, 5), pady=(5, 10))
+        close_settings_button = Button(settings_button_frame, text="Close", command=close_settings_menu)
+        close_settings_button.grid(row=0, column=3, sticky=W, padx=(5, 10), pady=(5, 10))
 
     def valid_number_test(self, source_input, is_float=True, entry_name='Entry'):
         if is_float:
@@ -130,7 +217,7 @@ class MainGUI:
             weight_based_price = False
         item_category = self.category_box.get()
         self.transaction_box.clear()
-        self.transaction_dict.setdefault(item_name, [item_price, item_quantity, item_category, weight_based_price])
+        self.transaction_dict.setdefault(item_name, [item_price, item_quantity, item_category, weight_based_price, item_weight])
         self.fill_transaction_box(self.transaction_dict)
         self.item_entry.delete(0, END)
         self.price_entry.delete(0, END)
@@ -143,7 +230,10 @@ class MainGUI:
         if len(self.transaction_dict) < 1:
             print('Empty Transaction')
             return
+        store = self.store_box.get()  # Add idiot tests.
+        date = self.date_entry.get()
         transaction_list = []
+        transaction_total = 0
         for key, value in self.transaction_dict.items():
             group_listing = [key.lower()]
             group_listing.extend(value)
@@ -152,10 +242,10 @@ class MainGUI:
             group_listing[1] = group_listing[1].replace('$', '')
             transaction_list.append(group_listing)
         for i in transaction_list:
-            if i[0] in self.name_list:
-                self.DBM.update_product(i)
-            else:
-                self.DBM.push_product(i)
+            self.DBM.update_product(i) if i[0] in self.name_list else self.DBM.push_product(i)
+            self.DBM.push_transaction(i, self.active_user, store, date)
+            transaction_total += ((float(i[1]) * float(i[5])) * int(i[2])) if i[4] else (float(i[1]) * int(i[2]))
+        self.DBM.push_transaction_summary(self.active_user, store, date, transaction_total)
         self.DBM.save()
         self.transaction_dict.clear()
         self.name_list = self.DBM.get_product_name_list()
